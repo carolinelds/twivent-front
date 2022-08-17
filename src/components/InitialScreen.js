8c294b4b0df9d3b54d3cac4adf1cb1ad7d9934a0
carolinelds <carolinelds@gmail.com>
2022-08-16 21:39:27 -0300

    feat: add initial layout of homepage

--- src/components/InitialScreen.js
+++ src/components/InitialScreen.js
@@ -1,15 +1,61 @@
 import styled from "styled-components";
+import "./../assets/css/fonts.css";
+import TextField from "@mui/material/TextField";
+import SearchIcon from '@mui/icons-material/Search';
+import Button from '@mui/material/Button';
 
-export default function InitialScreen(){
-    return (
-        <Div>
-            <h1>TESTE</h1>
-        </Div>
-    )
+export default function InitialScreen() {
+  return (
+    <Div>
+     
+        <div class="search">
+          <TextField id="outlined-basic" label="Chave do evento" variant="outlined" />
+          <Button variant="contained" disableElevation sx={{ backgroundColor: "#5b8291", opacity: 0.7 }}>
+            <SearchIcon />
+          </Button>  
+        </div>
+        <p>Criar evento</p> 
+    </Div>
+  );
 }
 
 const Div = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-top: 60px;
+  padding-top: 80px;
+  width: 100%;
+
+  p {
+    margin-top: 80px;
+    font-size: 20px;
+    font-family: "Roboto", sans-serif;
+  }
+
+  .search {
+    width: 85%;
     display: flex;
-    flex-direction: column;
-    
+    justify-content: center;
+    align-content: center;
+  }
+
+  button {
+    margin-left: 20px;
+  }
+
+  button:hover {
+    background-color: #5b8291;
+    opacity: 1;
+  }
+
+  .MuiFormControl-root,
+  .MuiTextField-root {
+    width: 100%;
+  }
+
+  p {
+    text-decoration: underline;
+  }
 `;
